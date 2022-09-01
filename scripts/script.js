3ef0507a60e1a42ceb38a0e250ea94f7691bef16
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-01 15:00:20 -0400

    Create functions for grid to size  and add onchange function to slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,39 @@
-// Create the grid
-const grid = document.querySelector(".grid");
-grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-grid.style.gridTemplateRows = "repeat(16, 1fr)";
 
-for(let i = 0; i < 256; i++) {
+// Create the grid
+function makeGrid(size){
+    const grid = document.querySelector(".grid");
+    
+    // Create columns and rows in grid
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    // Loop will create rows and columns and append them to the grid
+    for(let i = 0; i < (size**2); i++) {
     let gridSquare = document.createElement("div");
     gridSquare.style.backgroundColor = "red";
     grid.insertAdjacentElement("beforeend", gridSquare);
+    }
 }
 
 // Add dynamic sizing to grid
+function createGrid() {
+    let gridSize = document.querySelector(".slider")
+    makeGrid(gridSize.value);
+}
+
+createGrid()
+
 // allow user to click squares in grid and change colors
 // Also allow user to reset and eraser grid squares
+
+
+
+
+
+
+
+
+
+
+
 
 
