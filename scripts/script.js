f4626c769019ac144c1d43adad992bbe2fe97d07
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-01 20:28:51 -0400

    Add coloring function that user can click on and off

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,55 @@
+let click = true;
+
 
 // Create the grid
 function makeGrid(size){
     const grid = document.querySelector(".grid");
     
     // Create columns and rows in grid
+
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
     // Loop will create rows and columns and append them to the grid
     for(let i = 0; i < (size**2); i++) {
     let gridSquare = document.createElement("div");
-    gridSquare.style.backgroundColor = "red";
+    gridSquare.classList.add("square");
+    
+    gridSquare.addEventListener("mouseover", draw);  
     grid.insertAdjacentElement("beforeend", gridSquare);
     }
 }
+
 
 // Add dynamic sizing to grid
 function createGrid() {
     let gridSize = document.querySelector(".slider")
     makeGrid(gridSize.value);
+
 }
 
-createGrid()
 
+
+createGrid()
 // allow user to click squares in grid and change colors
+function draw() {
+    if(click) {
+        this.style.backgroundColor = "black";
+    }
+
+    }     
+
+document.querySelector("body").addEventListener("click", function() {
+   if(click) {
+    click = false;
+   } 
+   else {
+    click = true;
+   }
+})
+
+
+
 // Also allow user to reset and eraser grid squares
 
 
